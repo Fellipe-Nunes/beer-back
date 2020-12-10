65cfb9c284b233e109046ba06068c614ab9dfe45
Fellipe Nunes <fellipenunes@Macbook-Pro-de-Fellipe-Nunes.local>
2020-12-10 11:57:52 -0300

    back

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const connectDB = require('./config/db')
 const cors = require('cors')
 const app = express()
+const port = process.env || 3000
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -15,4 +16,4 @@ app.use('/product', require('./routes/api/product'))
 app.use('/auth', require('./routes/api/auth'))
 app.use('/user', require('./routes/api/user'))
 
-app.listen(3000, () => console.log("Connected"))
+app.listen(port, () => console.log("Connected"))
