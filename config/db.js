c18a7f098c853d281aa0cd363d23730ca1f1d8a4
Fellipe Nunes <fellipenunes@Macbook-Pro-de-Fellipe-Nunes.local>
2020-12-14 10:35:39 -0300

    mongo

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 const config = require('config')
-const db = config.get('mongoURI') || process.env.mongoURI
+//const db = config.get('mongoURI') || process.env.mongoURI
 
-const connectDB = async () => {
+const connectDB = async (db=process.env.mongoURI || config.get('mongoURI')) => {
     try {
 
         await mongoose.connect(db, {
